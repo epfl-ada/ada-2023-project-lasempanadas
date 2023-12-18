7ab2a3ee082248244a5d761606c2d995745df044
RemiOddon <remioddon74@gmail.com>
2023-12-18 03:54:10 +0100

    Feat + network analysis v1

--- download_data.sh
+++ download_data.sh
@@ -12,3 +12,7 @@ mv title.ratings.tsv title_ratings.tsv
 git clone https://github.com/MichelDucartier/ACL_personas_dump.git
 cd ACL_personas_dump/personas
 gunzip 25.100.lda.log.txt.gz
+git clone https://github.com/epfl-ada/ada-2023-project-draco.git
+mv ada-2023-project-draco/Data/ethnicities_data.tsv ethnicities_data.tsv 
+mv ada-2023-project-draco/Data/ethnicity_group_data.tsv ethnicity_group_data.tsv 
+rm -Rf ada-2023-project-draco
